9d995c653ae4bb432e70805082f98fb1c45b59af
Peter Kong <peter@peterkong.com>
2014-10-11 18:59:59 -0400

    fixing formatting

--- prettyPrint.js
+++ prettyPrint.js
@@ -1,41 +1,40 @@
 /*
-		prettyPrint prints javascript objects in an easily readable way
-		@author Peter Kong
+    prettyPrint prints javascript objects in an easily readable way
+    @author Peter Kong
 */
 
 (function(options){
 
-	var _config = {
-		//defaults
-		namespace: Window,
-		logger: console.log,
-	}
+  var _config = {
+    //defaults
+    namespace: Window,
+    logger: console.log,
+  }
 
-	if(typeof(options) === "object"){
-		if(options['namespace']){
-			_config['namespace'] = options['namespace'];
-		}
-		if(options['logger']){
-			_config['logger'] = options['logger'];
-		}
-	}
+  if(typeof(options) === "object"){
+    if(options['namespace']){
+      _config['namespace'] = options['namespace'];
+    }
+    if(options['logger']){
+      _config['logger'] = options['logger'];
+    }
+  }
+  
+  _config['namespace'].prettyPrint = function(obj, offset){
+    if (typeof(offset) === "undefined"){
+      offset = "";
+    }
+    for (var p in obj){
+      if (obj.hasOwnProperty(p)){
+        if (typeof(obj[p]) == "object"){
+          _config['logger'](offset + p + ":");
+          _config['namespace'].prettyPrint(obj[p], offset.concat("  "));
+        }
+        else {
+          _config['logger'](offset + p + ":" + obj[p]);
+        }
+      }
+    }
+  }
 
-	_config['namespace'].prettyPrint = function(obj, offset){
-	  if (typeof(offset) === "undefined"){
-	      offset = "";
-	  }
-	  for (var p in obj){
-	    if (obj.hasOwnProperty(p)){
-
-	      if (typeof(obj[p]) == "object"){
-	        _config['logger'](offset + p + ":");
-	        _config['namespace'].prettyPrint(obj[p], offset.concat("  "));
-	      }
-	      else {
-	        _config['logger'](offset + p + ":" + obj[p]);
-	      }
-	    }
-	  }
-	}
-
-})(options)
+})()
